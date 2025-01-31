e40dcee79ed88cbe9fffbaa05b52443bb070b949
zucy-9704 <akvirgen@ucdavis.edu>
2025-01-30 18:57:56 -0800

    update

---
+++ unit1/max3_practice.py
@@ -0,0 +1,12 @@
+# 12phred.py by Zucy
+
+def max3(x, y, z): 
+	if x > y > z: return x
+	elif y > z > x: return y
+	elif z > x > y: return z
+	elif z > y > x: return z
+	elif x > z > y: return x
+	elif y > x > z: return y
+	else: return
+	
+print(max3(3, 5, 7))
