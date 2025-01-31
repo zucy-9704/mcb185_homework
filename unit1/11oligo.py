62b18ba76ca8d4a9f197c09a5baef7b011271bd6
zucy-9704 <akvirgen@ucdavis.edu>
2025-01-30 18:18:02 -0800

    update

---
+++ unit1/11oligo.py
@@ -0,0 +1,6 @@
+def tm(A, T, C, G):
+	if A + T + C + G <= 13: return (A + T)*2 + (G + C)*4
+	else: return 64.9 + 41*(G + C - 16.4)/(A + T + G + C)
+	
+print(tm(4, 5, 2, 8))
+print(tm(38, 29, 48, 23))
