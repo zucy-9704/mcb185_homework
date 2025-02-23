2e6f5c58456a377965e620eec93f0558fdd128cb
zucy-9704 <akvirgen@ucdavis.edu>
2025-02-23 14:40:13 -0800

    update

--- unit2/25deathsaves.py
+++ unit2/25deathsaves.py
@@ -1,3 +1,36 @@
 # deathsaves.py	by Zucy
 
 import random
+
+health = 0
+n = 99
+pd = 0
+ps = 0
+pr = 0
+
+for rounds in range(n):
+	while True:
+		d20 = random.randint(1, 20)
+		if d20 == 1: health -= 2
+		elif d20 == 20: 
+			pr += 1
+			print('You are revived!')
+			health = 0
+			break
+		elif d20 >= 10: health += 1
+		elif d20 < 10: health -= 1
+		if health == 3: 
+			ps += 1
+			print('You are stable but unconscious')
+			health = 0
+			break
+		if health <= -3: 
+			pd += 1
+			print('You died!! :(')
+			health = 0
+			break
+	
+print('Probability one dies, stabilizes, or revives:')
+print('Dying:', pd/n)
+print('Stabilizing:', ps/n)
+print('Reviving:', pr/n)
