1928feb4fc9a65f868e3a7de2b6fa38721af7fa8
zucy-9704 <akvirgen@ucdavis.edu>
2025-02-27 21:10:28 -0800

    update

--- unit3/35scoringmatrix.py
+++ unit3/35scoringmatrix.py
@@ -6,13 +6,13 @@ nts = sys.argv[1]
 matches = sys.argv[2]
 mismatches = sys.argv[3]
 
-print('   ', end = '')
+print('   ', end = '')	# spacing in front of the header
 
-for nt in nts:
+for nt in nts:	# header
 	print(nt, end = '  ')
-print()
+print()	# inserts a newline character
 
-for nt1 in nts:
+for nt1 in nts:	# pairwise comparison
 	print(nt1, end = ' ')
 	for nt2 in nts:
 		if nt1 == nt2: print(matches, end = ' ')
