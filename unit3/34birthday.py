3cfb73bbd030b6bc1ae4b5b2c3ea67056b240944
zucy-9704 <akvirgen@ucdavis.edu>
2025-02-27 20:55:32 -0800

    update

--- unit3/34birthday.py
+++ unit3/34birthday.py
@@ -4,14 +4,30 @@ import sys
 import random
 
 trials = int(sys.argv[1])
-ppl = int(sys.argv[2])
-
-for i in range(0, 365):	# making the calendar
-		calendar = []
-		calendar.append(i)
+days = int(sys.argv[2])
+ppl = int(sys.argv[3])
+success = 0
 
 for t in range(0, trials):
+	# make calendar
+	calendar = []
+	for i in range(0, 365):	# making the calendar
+		calendar.append(0)
+		
+	# fill calendar
 	for j in range(0, ppl):
 		calendar[random.randint(0, len(calendar)-1)] += 1
 		
-print(calendar)
+	# check for bday collisions
+	sharedbdays = False
+	for i in range(len(calendar)):
+		if calendar[i] > 1: sharedbdays = True
+	if sharedbdays: success += 1
+	
+print(success/trials)
+
+
+
+
+
+
